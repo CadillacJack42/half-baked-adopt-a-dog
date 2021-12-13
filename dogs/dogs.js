7230dc06944e3653fb5d9e3e947644c55d40d724
James Keicher <jamesdkeicher@gmail.com>
2021-12-13 12:59:47 -0800

    saving progress. Working on tests

---
+++ dogs/dogs.js
@@ -0,0 +1,19 @@
+import { renderDogDetail } from '../render-utils.js';
+import { getDog } from '../fetch-utils.js';
+
+window.addEventListener('load', async() => {
+
+    const dogContainerEl = document.getElementById('dog-container');
+
+    const id = new URLSearchParams(window.location.search).get('id');
+    
+    const dog = await getDog(id);
+    console.log(dog);
+    
+    const dogEl = renderDogDetail(dog);
+
+    dogContainerEl.append(dogEl);
+
+
+});
+
--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,6 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { renderDogCard, renderDogDetail } from '../render-utils.js';
 
 const test = QUnit.test;
 
@@ -15,4 +16,14 @@ test('time to test a function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+test('should return a dom element that contains a list of dogs', (expect) => {
+    const dog = { id: 1, created_at: '2021-11-18T22:50:00+00:00', name: 'Bennie', age: 2, breed: 'beagle', description: 'Puggo big ol extremely cuuuuuute noodle horse shoober, porgo doge woofer. Snoot maximum borkdrive pupperino very jealous pupper woofer you are doin me a concern pupperino shibe heckin, corgo aqua doggo doggo long doggo bork puggo. H*ck long doggo adorable doggo super chub fat boi, waggy wags thicc. Doggo shooberino such treat, lotsa pats.' };
+
+    const expected = `<a href="./dogs/?id=1"><div class="dog-card"><p>Bennie</p><img src="./assets/beagle.jpeg"></div></a>`;
+
+    const actual = renderDogCard(dog);
+
+    expect.equal(actual.outerHTML, expected);
 });
